d6dc1fddf31e494aba76bdc3b48ba2460c9539f8
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-07 20:18:45 -0700

    display meetup on the moblie

--- meetupme-moblie/App.js
+++ meetupme-moblie/App.js
@@ -1,52 +1,51 @@
+import Expo, { AppLoading } from 'expo';
+
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import { fetchMeetups } from './constants/api';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Colors from './constants/Colors';
+import { HomeScreen } from './src/screens';
+import { cachedFonts } from './helpers';
 
-export default class App extends React.Component {
-  static defaultProps = {
-    fetchMeetups
-  };
+EStyleSheet.build(Colors);
 
+class App extends React.Component {
   state = {
-    loading: false,
-    meetups: []
+    fontLoaded: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const data = await this.props.fetchMeetups();
-    this.setState({ loading: false, meetups: data.meetups });
-    // setTimeout(
-    //   () => this.setState({ loading: false, meetups: data.meetups }),
-    //   10000
-    // );
+  componentDidMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    const fontAssets = cachedFonts([
+      {
+        montserrat: require('./assets/fonts/Montserrat-Regular.ttf')
+      },
+      {
+        montserratBold: require('./assets/fonts/Montserrat-Bold.ttf')
+      },
+      {
+        montserratLight: require('./assets/fonts/Montserrat-Light.ttf')
+      },
+      {
+        montserratMed: require('./assets/fonts/Montserrat-Medium.ttf')
+      }
+    ]);
+
+    await Promise.all(fontAssets);
+
+    this.setState({ fontLoaded: true });
   }
 
   render() {
-    if (this.state.loading) {
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator size="large" />
-        </View>
-      );
+    if (!this.state.fontLoaded) {
+      return <AppLoading />;
     }
 
-    return (
-      <View style={styles.container}>
-        <Text>MeetupME</Text>
-        {this.state.meetups.map((meetup, i) => (
-          <Text key={i}>{meetup.title}</Text>
-        ))}
-      </View>
-    );
+    return <HomeScreen />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+export default App;
+// Expo.registerRootComponent(App);
--- meetupme-moblie/constants/api.js
+++ meetupme-moblie/constants/api.js
@@ -1,2 +1,20 @@
-export const fetchMeetups = () =>
-  fetch('http://localhost:3000/api/meetups').then(res => res.json());
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+const fakeGroupId = '59d9443f16ea181e3b096ee4';
+
+class MeetupApi {
+  constructor() {
+    this.groupId = fakeGroupId;
+    this.path = `/groups/${this.groupId}/meetups`;
+  }
+
+  async fetchGroupMeetups() {
+    const { data } = await axios.get(this.path);
+
+    return data.meetups; //meetups from the model
+  }
+}
+
+export { MeetupApi };
